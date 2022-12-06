03d20de39a98f746090d10bad600029d73d3e98d
Loise <loise.wambui@student.moringaschool.com>
2022-12-06 15:41:38 +0300

    Modified elements in dom

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+document.querySelector("main").remove();
+
+//has a 'newHeader' variable that points to node 'h1#victory
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "YOUR-NAME is the champion";
